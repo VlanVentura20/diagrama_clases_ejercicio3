ca783c3a3b3490c6d4b90aaedece8b0114a83a4a
VlanVentura20 <izan.venturav@gmail.com>
2026-01-22 17:18:47 +0100

    añadir principal

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Customer cliente = new Customer();
+		cliente.name = "Izan";
+		cliente.surname = "Ventura";
+		cliente.age = 21;
+		
+		System.out.println(cliente.name + " " + cliente.surname + " " + cliente.age);
+		
+		
+	}
+	
+}
