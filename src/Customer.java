f0bd621d751be22044a970e58b4639537d522856
VlanVentura20 <izan.venturav@gmail.com>
2026-01-22 16:43:04 +0100

    ejercicio 3

---
+++ src/Customer.java
@@ -0,0 +1,22 @@
+
+public class Customer {
+
+	public String name;
+	public String surname;
+	protected int age;
+	private boolean single;
+	private String country;
+	
+	public void Customer() {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
+
+	private void soSomething() {
+		
+	}
+	
+}
